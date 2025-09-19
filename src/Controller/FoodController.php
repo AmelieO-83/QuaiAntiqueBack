400a82896d4e887527ebc8100681edf894cc466f
Amélie OLTZ <oltz.amelie@gmail.com>
2025-09-19 10:02:57 +0200

    Serializer + Nelmio

--- src/Controller/FoodController.php
+++ src/Controller/FoodController.php
@@ -7,74 +7,88 @@ use App\Repository\FoodRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
-#[Route('api/food', name:'app_api_food_')]
+#[Route('/api/food', name:'app_api_food_')]
 final class FoodController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $manager, private FoodRepository $repository)
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private FoodRepository $repository,
+        private SerializerInterface $serializer,
+        private UrlGeneratorInterface $urlGenerator,)
     {
     }
 
     #[Route(name:'new', methods: ['POST'])]
-    public function new(): Response
+    public function new(Request $request): JsonResponse
     {
-        $food = new Food();
-        $food->setTitle('Lasagne');
-        $food->setDescription('Lasagne maison');
-        $food->setPrice(19);
+        $food = $this->serializer->deserialize($request->getContent(), Food::class, 'json');
         $food->setCreatedAt(new DateTimeImmutable());
 
         $this->manager->persist($food);
         $this->manager->flush();
 
-        return $this->json(
-            ['message' => "Food resource created with {$food->getId()} id"],
-            Response::HTTP_CREATED,
+        $responseData = $this->serializer->serialize($food, 'json');
+        $location = $this->urlGenerator->generate(
+            'app_api_food_show',
+            ['id' => $food->getId()],
+            UrlGeneratorInterface::ABSOLUTE_URL,
         );
+        
+        return new JsonResponse ($responseData, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 
     #[Route('/{id}', name: 'show', methods: ['GET'])]
-    public function show(int $id): Response
+    public function show(int $id): JsonResponse
     {
         $food = $this->repository->findOneBy(['id' => $id]);
-
-        if (!$food) {
-            throw $this->createNotFoundException("No Food found for {$id} id");
+        if ($food) {
+            $responseData = $this->serializer->serialize($food, format: 'json');
+            
+            return new JsonResponse ($responseData, Response::HTTP_OK, [], true);
         }
 
-        return $this->json(
-            ['message' => "A Food was found : {$food->getTitle()} for {$food->getId()} id"]
-        );
+        return new JsonResponse (null, Response::HTTP_NOT_FOUND);
     }
 
     #[Route('/{id}', name: 'edit', methods: ['PUT'])]
-    public function edit(int $id): Response
+    public function edit(int $id, Request $request): JsonResponse
     {
         $food = $this->repository->findOneBy(['id' => $id]);
+        if ($food) {
+            $food = $this->serializer->deserialize(
+                $request->getContent(),
+                Food::class,
+                'json',
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $food]
+            );
+            $food->setUpdatedAt(new DateTimeImmutable());
+            $this->manager->flush();
 
-        if (!$food) {
-            throw $this->createNotFoundException("No Food found for {$id} id");
+            return new JsonResponse (data:null, status:Response::HTTP_NO_CONTENT);
         }
 
-        $food->setTitle('Food name updated');
-        $this->manager->flush();
-
-        return $this->redirectToRoute('app_api_food_show', ['id' => $food->getId()]);
+        return new JsonResponse (data:null, status:Response::HTTP_NOT_FOUND);
     }
 
     #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
-    public function delete(int $id): Response
+    public function delete(int $id): JsonResponse
     {
         $food = $this->repository->findOneBy(['id' => $id]);
-        if (!$food) {
-            throw $this->createNotFoundException("No Food found for {$id} id");
-        }
+        if ($food) {
+            $this->manager->remove($food);
+            $this->manager->flush();
 
-        $this->manager->remove($food);
-        $this->manager->flush();
+            return new JsonResponse (data:null, status:Response::HTTP_NO_CONTENT);
+        } 
 
-        return $this->json(['message' => "Food resource deleted"], Response::HTTP_NO_CONTENT);
+        return new JsonResponse (data:null, status:Response::HTTP_NOT_FOUND);
     }
 }
